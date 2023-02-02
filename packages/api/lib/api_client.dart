ab3b4f6666a68e0f32453f86c3a932cba5002b89
sunuma <sunuma0320@gmail.com>
2023-02-02 14:55:06 +0900

    マルチパッケージ構成のプロジェクトディレクトを作成

---
+++ packages/api/lib/api_client.dart
@@ -0,0 +1,5 @@
+library api;
+
+class ApiClient {
+
+}
---
+++ packages/app/android/app/src/main/kotlin/jp/sunuma/gagaga/product/simple_ai_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package jp.sunuma.gagaga.product.simple_ai_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
