eb4fb4ac45107da03d933e36b225c55054ea612d
sunuma <sunuma0320@gmail.com>
2023-02-12 15:15:35 +0900

    commonにカラー定義用のファイルを追加

---
+++ packages/common/lib/colors.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  final bool _isDark;
+
+  ThemeColors(BuildContext context) :
+        _isDark = MediaQuery.platformBrightnessOf(context) == Brightness.dark;
+
+  ThemeData dark() {
+    final themeData = ThemeData.dark();
+    return themeData.copyWith(
+        colorScheme: themeData.colorScheme.copyWith(
+          brightness: Brightness.dark,
+          primary: DarkColors.primary.color,
+          onPrimary: DarkColors.textPrimary.color,
+          secondary: DarkColors.secondary.color,
+          onSecondary: DarkColors.textSecondary.color,
+          error: DarkColors.fieldError.color,
+          onError: DarkColors.textPrimary.color,
+          background: LightColors.background.color,
+          onBackground: DarkColors.textPrimary.color,
+          surface: DarkColors.background.color,
+          onSurface: DarkColors.textPrimary.color,
+        ),
+        //elevatedButtonTheme: _elevatedButtonThemeData(),
+        //textButtonTheme: _textButtonThemeData(),
+        //inputDecorationTheme: inputDecorationTheme(),
+        //checkboxTheme: _checkboxThemeData()
+    );
+  }
+}
+
+enum DarkColors {
+  primary(value: 0xff499f80),
+  secondary(value: 0xff499f80),
+  textPrimary(value: 0xffececf1),
+  textSecondary(value: 0xff9b9b9f),
+  textMenu(value: 0xffffffff),
+  fieldBorder(value: 0xff2A2b39),
+  fieldError(value: 0xffbf2c25),
+  fieldHint(value: 0xff8e8e9e),
+  background(value: 0xff343540),
+  panelBackground(value: 0xff3e3f4a),
+  sideMenuBackground(value: 0xff202123),
+  required(value: 0xffbf2c25);
+
+  const DarkColors({required this.value});
+
+  final int value;
+}
+
+enum LightColors {
+  primary(value: 0xff499f80),
+  secondary(value: 0xff499f80),
+  textPrimary(value: 0xffececf1),
+  textSecondary(value: 0xff9b9b9f),
+  textMenu(value: 0xffffffff),
+  fieldBorder(value: 0xff2A2b39),
+  fieldError(value: 0xffbf2c25),
+  fieldHint(value: 0xff8e8e9e),
+  background(value: 0xff343540),
+  panelBackground(value: 0xff3e3f4a),
+  sideMenuBackground(value: 0xff202123),
+  required(value: 0xffbf2c25);
+
+  const LightColors({required this.value});
+
+  final int value;
+}
+
+extension on DarkColors {
+  Color get color {
+    return Color(value);
+  }
+}
+
+extension on LightColors {
+  Color get color {
+    return Color(value);
+  }
+}
