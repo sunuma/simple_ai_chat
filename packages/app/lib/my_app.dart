3d4240f51ded80ada8a140413b47498f001a4358
sunuma <sunuma0320@gmail.com>
2023-02-12 16:26:28 +0900

    ローカライズ設定を追加

--- packages/app/lib/my_app.dart
+++ packages/app/lib/my_app.dart
@@ -1,10 +1,25 @@
+import 'package:common/colors.dart';
+import 'package:common/destinations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("My App");
+    final themeColors = ThemeColors(context);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: themeColors.lights(),
+      darkTheme: themeColors.dark(),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: Destination.chat.screen(),
+      routes: Map.fromIterables(
+        Destination.values.map((e) => e.routeName),
+        Destination.values.map((e) => (context) => e.screen())
+      ),
+    );
   }
 }
