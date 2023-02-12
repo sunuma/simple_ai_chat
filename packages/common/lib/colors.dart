d87c664b0ba9004d916a933b871f4b760d3603ea
sunuma <sunuma0320@gmail.com>
2023-02-12 16:27:34 +0900

    カラー設定を更新

--- packages/common/lib/colors.dart
+++ packages/common/lib/colors.dart
@@ -28,6 +28,29 @@ class ThemeColors {
         //checkboxTheme: _checkboxThemeData()
     );
   }
+
+  ThemeData lights() {
+    final themeData = ThemeData.light();
+    return themeData.copyWith(
+        colorScheme: themeData.colorScheme.copyWith(
+            brightness: Brightness.light,
+            primary: LightColors.primary.color,
+            onPrimary: LightColors.textPrimary.color,
+            secondary: LightColors.secondary.color,
+            onSecondary: LightColors.textSecondary.color,
+            error: LightColors.fieldError.color,
+            onError: LightColors.textPrimary.color,
+            background: LightColors.background.color,
+            onBackground: LightColors.textPrimary.color,
+            surface: LightColors.background.color,
+            onSurface: LightColors.textPrimary.color
+        ),
+        //elevatedButtonTheme: _elevatedButtonThemeData(),
+        //textButtonTheme: _textButtonThemeData(),
+        //inputDecorationTheme: inputDecorationTheme(),
+        //checkboxTheme: _checkboxThemeData()
+    );
+  }
 }
 
 enum DarkColors {
@@ -52,14 +75,14 @@ enum DarkColors {
 enum LightColors {
   primary(value: 0xff499f80),
   secondary(value: 0xff499f80),
-  textPrimary(value: 0xffececf1),
-  textSecondary(value: 0xff9b9b9f),
+  textPrimary(value: 0xff343540),
+  textSecondary(value: 0xff7f7f7f),
   textMenu(value: 0xffffffff),
   fieldBorder(value: 0xff2A2b39),
   fieldError(value: 0xffbf2c25),
   fieldHint(value: 0xff8e8e9e),
-  background(value: 0xff343540),
-  panelBackground(value: 0xff3e3f4a),
+  background(value: 0xffffffff),
+  panelBackground(value: 0xfff7f7f8),
   sideMenuBackground(value: 0xff202123),
   required(value: 0xffbf2c25);
 
