306cdcdb9e5cbc73d0cc7aea3f4dd12d334740ac
sunuma <sunuma0320@gmail.com>
2023-02-18 13:50:57 +0900

    テキストのスタイル毎のTextクラスを作成

---
+++ packages/common/lib/ui/text_extensions.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class HeadlineLarge extends Text {
+  HeadlineLarge(this.text,  this.context,  {super.key}): super(text, style: Theme.of(context).textTheme.headlineLarge);
+
+  final String text;
+  final BuildContext context;
+}
+
+class HeadlineMedium extends Text {
+  HeadlineMedium(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.headlineMedium);
+
+  final String text;
+  final BuildContext context;
+}
+
+class HeadlineSmall extends Text {
+  HeadlineSmall(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.headlineSmall);
+
+  final String text;
+  final BuildContext context;
+}
+
+class BodyLarge extends Text {
+  BodyLarge( this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.bodyLarge);
+
+  final String text;
+  final BuildContext context;
+}
+
+class BodyMedium extends Text {
+  BodyMedium(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.bodyMedium);
+
+  final String text;
+  final BuildContext context;
+}
+
+class BodySmall extends Text {
+  BodySmall(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.bodySmall);
+
+  final String text;
+  final BuildContext context;
+}
+
+class TitleLarge extends Text {
+  TitleLarge(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.titleLarge);
+
+  final String text;
+  final BuildContext context;
+}
+
+class TitleMedium extends Text {
+  TitleMedium(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.titleMedium);
+
+  final String text;
+  final BuildContext context;
+}
+
+class TitleSmall extends Text {
+  TitleSmall(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.titleSmall);
+
+  final String text;
+  final BuildContext context;
+}
+
+class DisplayLarge extends Text {
+  DisplayLarge(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.displayLarge);
+
+  final String text;
+  final BuildContext context;
+}
+
+class DisplayMedium extends Text {
+  DisplayMedium(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.displayMedium);
+
+  final String text;
+  final BuildContext context;
+}
+
+class DisplaySmall extends Text {
+  DisplaySmall(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.displaySmall);
+
+  final String text;
+  final BuildContext context;
+}
+
+class LabelLarge extends Text {
+  LabelLarge(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.labelLarge);
+
+  final String text;
+  final BuildContext context;
+}
+
+class LabelMedium extends Text {
+  LabelMedium(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.labelMedium);
+
+  final String text;
+  final BuildContext context;
+}
+
+class LabelSmall extends Text {
+  LabelSmall(this.text, this.context, {super.key}): super(text, style: Theme.of(context).textTheme.labelSmall);
+
+  final String text;
+  final BuildContext context;
+}
