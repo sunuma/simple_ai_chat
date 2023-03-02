c9748de75f1b42166aead0daedf55a4e1c2a3219
sunuma <sunuma0320@gmail.com>
2023-03-02 17:30:34 +0900

    main.dartを移動

--- packages/app/lib/main.dart
+++ packages/app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'my_app.dart';
+import 'ui/my_app.dart';
 
 void main() {
   runApp(const ProviderScope(
